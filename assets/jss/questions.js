81595bd4b6f100f0d7c71f7065c490de956b3bc6
jxdang1 <dangjennifer21@gmail.com>
2023-10-13 00:13:32 -0500

    created files needed for challenge

---
+++ assets/jss/questions.js
@@ -0,0 +1,9 @@
+//created question variable to have an array of each question, choices, and answers
+
+var questions = [
+    {
+      title: "Which one is a looping structure in JavaScript?",
+      choices: ["All the below", "For", "While", "do-while loops"],
+      answer: "All the below"
+    },
+]
